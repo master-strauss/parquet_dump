8350a3960b6372ee1cbd6df8b96caa8bd3109189
Marco Aurelio Lotz <contact@marcolotz.com>
2022-08-20 23:09:34 +0200

    first commit with gradle and open api configured

---
+++ src/main/java/com/marcolotz/db2parquet/Db2parquetApplication.java
@@ -0,0 +1,13 @@
+package com.marcolotz.db2parquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2parquetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db2parquetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcolotz/db2parquet/Db2parquetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcolotz.db2parquet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db2parquetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
