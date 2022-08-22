b08eda37dfcbac362d30faf83ecca6540470eb64
Marco Aurelio Lotz <contact@marcolotz.com>
2022-08-22 18:14:20 +0200

    config fix

--- src/main/java/com/marcolotz/db2parquet/config/AdaptersConfig.java
+++ src/main/java/com/marcolotz/db2parquet/config/AdaptersConfig.java
@@ -1,12 +1,13 @@
 package com.marcolotz.db2parquet.config;
 
-import com.marcolotz.db2parquet.core.IngestionCoordinator;
 import com.marcolotz.db2parquet.core.BaseIngestionService;
+import com.marcolotz.db2parquet.core.IngestionCoordinator;
 import com.marcolotz.db2parquet.port.IngestionService;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @EnableConfigurationProperties
@@ -27,6 +28,20 @@ public class AdaptersConfig {
   @ConfigurationProperties(prefix = "db2parquet")
   public Db2ParquetConfigurationProperties db2ParquetConfigurationProperties() {
     return new Db2ParquetConfigurationProperties();
+  }
+
+  // Design decision:
+  // In order to keep it generic on db technology, table schema and to clearly control heap-pollution
+  // I decided to implement this directly instead of abstracting (e.g. JdbcTemplate).
+  @Bean
+  DriverManagerDataSource driverManagerDataSource(final Db2ParquetConfigurationProperties configurationProperties)
+  {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(configurationProperties.getJdbc().getDriverClass());
+    dataSource.setUrl(configurationProperties.getJdbc().getUrl());
+    dataSource.setUsername(configurationProperties.getJdbc().getUserName());
+    dataSource.setPassword(configurationProperties.getJdbc().getPassword());
+    return dataSource;
   }
 
 }
